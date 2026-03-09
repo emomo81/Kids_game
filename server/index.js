ae647ba11d3846d4113f6bdcc3ff7385e4d01aac
emomo81 <emmanuelmomo81@gmail.com>
2026-03-10 00:39:15 +0200

    Update server/index.js

--- server/index.js
+++ server/index.js
@@ -3,16 +3,23 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
+import entitiesRoutes from './routes/entities.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/entities', entitiesRoutes);
 
 app.get('/', (req, res) => {
     res.send('Kids Game API is running!');
@@ -37,3 +44,9 @@ mongoose.connect(MONGO_URI)
     .catch((error) => {
         console.error('Error connecting to MongoDB:', error.message);
     });
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    console.error('Unhandled Error:', err);
+    res.status(500).json({ message: 'Internal Server Error' });
+});
